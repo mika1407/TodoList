db1238a2c5f67f2c7327b99d3584338e997b3330
mika1407 <koneautomaatio@hotmail.com>
2020-01-30 00:01:23 +0200

    Updates

---
+++ precache-manifest.fab0abc4e82ce5d5f1671e1cd8bf012a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c5b377e80a624f05e1aa0e18560949",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "93b752f32b0c569963a0",
+    "url": "/TodoList/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "27d3308eb8532e9d6ee3",
+    "url": "/TodoList/static/js/2.64335a1f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/TodoList/static/js/2.64335a1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93b752f32b0c569963a0",
+    "url": "/TodoList/static/js/main.aa309ff8.chunk.js"
+  },
+  {
+    "revision": "f252b29a44529df1f414",
+    "url": "/TodoList/static/js/runtime-main.e2728836.js"
+  }
+]);
