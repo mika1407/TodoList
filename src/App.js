7061234159d6a1d64471084855e489592c388439
mika1407 <koneautomaatio@hotmail.com>
2020-01-29 04:00:24 +0200

    added styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import Todos from "./components/Todos";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner with wife",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "Meeting with boss",
+        completed: false
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
